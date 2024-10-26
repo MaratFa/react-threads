db01f14473bd4f400d1dff58f54a2e2f940fa3d0
MaratFa <evolution99@rambler.ru>
2024-10-27 00:16:31 +0300

    Edited

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,3 +1,20 @@
+import { Outlet } from "react-router-dom"
+import { Container } from "../container"
+import { Header } from "../header"
+import { NavBar } from "../nav-bar/index"
+
 export const Layout = () => {
-  return <>Layout</>
+  return (
+    <>
+      <Header />
+      <Container>
+        <div className="flex-2 p-4">
+          <NavBar />
+        </div>
+        <div className="flex-1 p-4">
+          <Outlet />
+        </div>
+      </Container>
+    </>
+  )
 }
