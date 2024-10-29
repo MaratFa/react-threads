b6c7dd95ec43d352acd5167a1e62704bc5fe4339
MaratFa <evolution99@rambler.ru>
2024-10-30 00:05:58 +0300

    Added

---
+++ src/features/register.tsx
@@ -0,0 +1,80 @@
+import { useForm } from "react-hook-form"
+import { Input } from "../components/input"
+import { Button, Link } from "@nextui-org/react"
+
+type Register = {
+  email: string
+  name: string
+  password: string
+}
+
+type Props = {
+  setSelected: (value: string) => void
+}
+
+export const Register = ({ setSelected }: Props) => {
+  const {
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm<Login>({
+    mode: "onChange",
+    reValidateMode: "onBlur",
+    defaultValues: {
+      email: "",
+      password: "",
+      name: "",
+    },
+  })
+
+
+
+
+
+  
+  const onSubmit = async (data: Register) => {
+    try {
+    } catch (error) {}
+  }
+
+  return (
+    <form className="flex flex-col gap-4" onSubmit={handleSubmit(onSubmit)}>
+      <Input
+        control={control}
+        name="name"
+        label="Имя"
+        type="text"
+        required="Обязательное поле"
+      />
+      <Input
+        control={control}
+        name="email"
+        label="Email"
+        type="email"
+        required="Обязательное поле"
+      />
+      <Input
+        control={control}
+        name="password"
+        label="Пароль"
+        type="password"
+        required="Обязательное поле"
+      />
+      <p className="text-center text-small">
+        Уже есть аккаунт?{" "}
+        <Link
+          size="sm"
+          className="cursor-pointer"
+          onPress={() => setSelected("login")}
+        >
+          Войдите
+        </Link>
+      </p>
+      <div className="flex gap-2 justify-end">
+        <Button fullWidth color="primary" type="submit" isLoading={isLoading}>
+          Войти
+        </Button>
+      </div>
+    </form>
+  )
+}
