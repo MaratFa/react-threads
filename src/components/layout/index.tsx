ce785af7e67facab6ff1f71a3a57f60031132d6f
MaratFa <evolution99@rambler.ru>
2024-11-02 06:55:40 +0300

    Edited

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,9 +1,22 @@
-import { Outlet } from "react-router-dom"
+import React, { useEffect } from "react"
 import { Container } from "../container"
 import { Header } from "../header"
 import { NavBar } from "../nav-bar/index"
+import { Outlet, useNavigate } from "react-router-dom"
+import { useSelector } from "react-redux"
+import { selectIsAuthenticated, selectUser } from "../../features/user/userSlice"
 
 export const Layout = () => {
+  const isAuthenticated = useSelector(selectIsAuthenticated);
+  const user = useSelector(selectUser);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!isAuthenticated) {
+      navigate('/auth')
+    }
+  }, [])
+  
   return (
     <>
       <Header />
