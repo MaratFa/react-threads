eaa39170cc455c8bba489317c5ad342eed254926
MaratFa <evolution99@rambler.ru>
2024-11-07 06:34:56 +0300

    Edited

--- src/components/user/index.tsx
+++ src/components/user/index.tsx
@@ -1,5 +1,28 @@
-export const User = () => {
+import type React from "react";
+import { User as NextUiUser } from "@nextui-org/react";
+import { BASE_URL } from "../../constants";
+
+type Props = {
+  name: string;
+  avatarUrl: string;
+  description?: string;
+  className?: string;
+};
+
+export const User: React.FC<Props> = ({
+  name = "",
+  description = "",
+  avatarUrl = "",
+  className = "",
+}) => {
   return (
-    <div>User</div>
-  )
-}
+    <NextUiUser
+      name={name}
+      className={className}
+      description={description}
+      avatarProps={{
+        src: `${BASE_URL}${avatarUrl}`,
+      }}
+    />
+  );
+};
