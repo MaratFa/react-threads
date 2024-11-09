9bc296d11fcb84205d0ff991633aa874aabf926c
MaratFa <evolution99@rambler.ru>
2024-11-09 14:34:08 +0300

    Edited

--- src/pages/current-post/index.tsx
+++ src/pages/current-post/index.tsx
@@ -1,3 +1,25 @@
+import { useParams } from "react-router-dom"
+import { useGetPostByIdQuery } from "../../app/services/postsApi";
+import { Card } from "../../components/card"
+import { createComment } from "../../components/";
+
+
+
 export const CurrentPost = () => {
+  const params = useParams<{ id: string}>();
+  const { data } = useGetPostByIdQuery(params?.id ?? "")
+
+  if (!data) {
+    return <h2>Пост не существует</h2>
+  }
+  
+  const {
+    content,
+    id,
+    authorId,
+    comments,
+    likedByUser
+  }
+
   return <div>CurrentPost</div>
 }
